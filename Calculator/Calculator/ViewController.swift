85cfcb97e0d566e4d669bca810be55a42c34e713
lubakozus <lubakozus@gmail.com>
2020-09-17 17:59:36 +0300

    Calculator init

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by ILLIA HARKAVY on 9/17/20.
+//  Copyright © 2020 ILLIA HARKAVY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
